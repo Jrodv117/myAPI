34c77e3aae1939506939e3eb2f6c2dac6ebfd473
jrodr117 <jrodr113@msudenver.edu>
2022-06-14 13:12:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+	console.log('run server, run');
+});
